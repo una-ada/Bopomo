5e226b5f7719754e32baf3e327e0f403719cdd75
Una <trewbot@gmail.com>
2021-08-02 17:19:06 -0700

    style(model): refactor user model as es2015

--- models/user.js
+++ models/user.js
@@ -1,60 +1,61 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+/**
+ * User Schema
+ * @author Una Ada <una@anarchy.website>
+ * @version 2021.08.02
+ * @since 2021.08.02
+ * @module models/user
+ * @see module:routes/users
+ * @see module:controllers/users
+ */
 
+/*----- Imports --------------------------------------------------------------*/
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+/*----- Initialize -----------------------------------------------------------*/
 const SALT_ROUNDS = 6;
 
-const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, lowercase: true, unique: true},
-  email: {type: String, required: true, lowercase: true, unique: true},
-  password: String,
-  photoUrl: String  // string from aws!
-}, {
-  timestamps: true
-});
+/*----- Schema ---------------------------------------------------------------*/
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, lowercase: true, unique: true },
+    email: { type: String, required: true, lowercase: true, unique: true },
+    password: String,
+    photoUrl: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
+/*----- Methods --------------------------------------------------------------*/
 userSchema.set('toJSON', {
-  transform: function(doc, ret) {
-    // remove the password property when serializing doc to JSON
+  transform: function (doc, ret) {
     delete ret.password;
     return ret;
-  }
+  },
 });
-/// in controller
-
-// this is if you populate the user
 userSchema.set('toObject', {
   transform: (doc, ret, opt) => {
-   delete ret.password;
-   return ret;
-  }
+    delete ret.password;
+    return ret;
+  },
 });
-
-
-// DO NOT DEFINE instance methods with arrow functions, 
-// they prevent the binding of this
-userSchema.pre('save', function(next) {
-  // 'this' will be set to the current document
+userSchema.pre('save', function (next) {
   const user = this;
-  // check to see if the user has been modified, if not proceed 
-  // in the middleware chain
   if (!user.isModified('password')) return next();
-  // password has been changed - salt and hash it
-  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
     if (err) return next(err);
-    // replace the user provided password with the hash
     user.password = hash;
     next();
   });
 });
-
-userSchema.methods.comparePassword = function(tryPassword, cb) {
-    console.log(cb, ' this is cb')
-  // 'this' represents the document that you called comparePassword on
-  bcrypt.compare(tryPassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-
-    cb(null, isMatch);
+userSchema.methods.comparePassword = function (tryPassword, next) {
+  bcrypt.compare(tryPassword, this.password, function (err, isMatch) {
+    if (err) return next(err);
+    next(null, isMatch);
   });
 };
 
-module.exports = mongoose.model('User', userSchema);
+/*----- Exports --------------------------------------------------------------*/
+export default mongoose.model('User', userSchema);
